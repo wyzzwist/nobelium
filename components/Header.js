f070e27190085b0c9ca323a549c6811fe3d1e1bd
yizhenw <wyzzwist@gmail.com>
2026-09-17 11:42:48 +0200

    Restore original header icon size

--- components/Header.js
+++ components/Header.js
@@ -129,10 +129,9 @@ export default function Header ({ navBarTitle, fullWidth }) {
           <Link href="/" aria-label={BLOG.title}>
             <Image
               src={favicon}
-              width={32}
-              height={32}
+              width={24}
+              height={24}
               alt={BLOG.title}
-              className="w-8 h-8 md:w-6 md:h-6 flex-shrink-0 object-contain"
               onError={() => setFavicon(true)}
             />
           </Link>
